db0495ddbd60bcbe03d4a0d30246541ee65b96a8
greenfan <rdwyer@fastmail.com>
2019-11-18 21:30:29 -0800

    Update netsnap.py

--- netsnap.py
+++ netsnap.py
@@ -1,44 +1,50 @@
-
 #!/usr/bin/python3
 # netsnapshot.py - network monitoring with scapy and python pandas
-# Release .01 - - Nov. 2019 - - Russell Dwyer www.russelldwyer.com
+# Release .11 - - Nov. 2019 - - Russell Dwyer www.russelldwyer.com
 
 from typing import Any, Union
 
 from scapy.all import *
 import os
+import sys
 import time
 import pandas as pd
-from scapy.plist import PacketList
 
+
+localbroadcast = os.system("ifconfig | grep broadcast | awk '{ print $6 }'")
 
 packets = sniff(count=10)
-wrpcap("/tmp/filename.pcap", packets)
-count = 1
 
+wrpcap("/tmp/filename.pcap", packets)
 
 scapy_cap = rdpcap("/tmp/filename.pcap")
 
 
 for packet in scapy_cap:
     PT = (packet.type)
-    proto = (packet[IP].proto)
-    count += 1
+
     if PT == 2048:
+        proto = (packet[IP].proto)
         if ( proto == 17) or ( proto == 6 ):
             df = pd.DataFrame({
-                "Column1": [(packet[IP].src)],
-                "Column2": [(packet[IP].dst)],
-                "Column3": [(packet[IP].dport)],
-                "Column4": [(packet[IP].sport)],
-                "Column5": [(len(packet))],
-                "#": [(count)]
+                "Source": [(packet[IP].src)],
+                "Destination": [(packet[IP].dst)],
+                "Destport": [(packet[IP].dport)],
+                "Sourceport": [(packet[IP].sport)],
+                "Size": [(len(packet))],
             })
-            print(df)
+        else: print(proto)
 
-        else: print(packet.summary, proto)
     elif PT == 34525:
-        print("ipv6")
-    elif PT == 2054:
-        print("ARP")
+        proto = (packet[IPv6].nh)
+        if ( proto == 17) or ( proto == 6 ):                                                                                                                         
+            df = pd.DataFrame({                                                                                                                                      
+                "Source": [(packet[IPv6].src)],                                                                                                                      
+                "Destination": [(packet[IPv6].dst)],                                                                                                                 
+                "Destport": [(packet[IPv6].dport)],
+                "Sourceport": [(packet[IPv6].sport)],
+                "Size": [(len(packet))],
+            })
 
+    elif ( PT == 2054):
+        print("ARP!")
