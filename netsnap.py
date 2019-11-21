c019f615e68e4f1beec283b06cea6ff79add2438
greenfan <rdwyer@fastmail.com>
2019-11-20 18:31:17 -0800

    commit v.13

--- netsnap.py
+++ netsnap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # netsnapshot.py - network monitoring with scapy and python pandas
-# Release .11 - - Nov. 2019 - - Russell Dwyer www.russelldwyer.com
+# Release  Alpha Alpha Beta
 
 from typing import Any, Union
 
@@ -9,15 +9,30 @@ import os
 import sys
 import time
 import pandas as pd
+from subprocess import PIPE, Popen
 
 
-localbroadcast = os.system("ifconfig | grep broadcast | awk '{ print $6 }'")
+def cmdline(command):
+    process = Popen(
+        args=command,
+        stdout=PIPE,
+        shell=True
+    )
+    return process.communicate()[0]
+
+
+
+
+
+
 
 packets = sniff(count=10)
 
-wrpcap("/tmp/filename.pcap", packets)
+wrpcap("/tmp/filename2.pcap", packets)
 
-scapy_cap = rdpcap("/tmp/filename.pcap")
+scapy_cap = rdpcap("/tmp/filename2.pcap")
+
+
 
 
 for packet in scapy_cap:
@@ -37,10 +52,10 @@ for packet in scapy_cap:
 
     elif PT == 34525:
         proto = (packet[IPv6].nh)
-        if ( proto == 17) or ( proto == 6 ):                                                                                                                         
-            df = pd.DataFrame({                                                                                                                                      
-                "Source": [(packet[IPv6].src)],                                                                                                                      
-                "Destination": [(packet[IPv6].dst)],                                                                                                                 
+        if ( proto == 17) or ( proto == 6 ):
+            df = pd.DataFrame({
+                "Source": [(packet[IPv6].src)],
+                "Destination": [(packet[IPv6].dst)],
                 "Destport": [(packet[IPv6].dport)],
                 "Sourceport": [(packet[IPv6].sport)],
                 "Size": [(len(packet))],
@@ -48,3 +63,26 @@ for packet in scapy_cap:
 
     elif ( PT == 2054):
         print("ARP!")
+
+    df.to_csv('/tmp/fulldump2.csv', mode='a',  sep='\t', header=False)
+
+
+
+col_Names=['Source', 'Destination', 'Destport', 'Sourceport', 'Size']
+
+
+Cov = pd.read_csv("/tmp/fulldump2.csv",  sep='\t', names=col_Names)
+
+
+#localbroadcast = os.popen("ifconfig | grep broadcast | awk '{ print $6 }'").read()
+
+
+localbroadcast = "192.168.0.255"
+
+
+Cov1 = Cov[Cov.Destination != (localbroadcast) ]
+
+Cov.to_csv('/tmp/headerdump.csv')
+Cov1.to_csv('/tmp/withoutbroadcasts.csv')
+#print(Cov1)
+
