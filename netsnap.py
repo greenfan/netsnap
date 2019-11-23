ea7dcb614566ecf2df7038f359524f8800e715ba
greenfan <rdwyer@fastmail.com>
2019-11-22 17:54:37 -0800

    commit v.12

--- netsnap.py
+++ netsnap.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # netsnapshot.py - network monitoring with scapy and python pandas
-# Release  Alpha Alpha Beta
+# Release Version:  Alpha Beta
+# Release Date: 11/22/19
 
 from typing import Any, Union
 
@@ -8,10 +9,13 @@ from scapy.all import *
 import os
 import sys
 import time
+import numpy as np
 import pandas as pd
 from subprocess import PIPE, Popen
-
-
+#
+#
+#
+# local functions
 def cmdline(command):
     process = Popen(
         args=command,
@@ -20,20 +24,23 @@ def cmdline(command):
     )
     return process.communicate()[0]
 
+#convert our output from local system ip into a usable python list
+def Convert(string):
+    li = list(string.split("\n"))
+    return li
+
+# local variables
+local_system_ip = cmdline(" ifconfig | grep -i inet | egrep -v \"fe80|::1|127.0.0\" | awk '{  print  $2 }' ").decode('ascii')
+localbroadcast = cmdline("ifconfig | grep broadcast | awk ' { print $6 }'").decode('ascii').strip()
 
 
 
-
-
-
-packets = sniff(count=10)
+# define length
+packets = sniff(count=300)
 
 wrpcap("/tmp/filename2.pcap", packets)
 
 scapy_cap = rdpcap("/tmp/filename2.pcap")
-
-
-
 
 for packet in scapy_cap:
     PT = (packet.type)
@@ -48,7 +55,7 @@ for packet in scapy_cap:
                 "Sourceport": [(packet[IP].sport)],
                 "Size": [(len(packet))],
             })
-        else: print(proto)
+        else: print(packet.summary)
 
     elif PT == 34525:
         proto = (packet[IPv6].nh)
@@ -64,25 +71,94 @@ for packet in scapy_cap:
     elif ( PT == 2054):
         print("ARP!")
 
-    df.to_csv('/tmp/fulldump2.csv', mode='a',  sep='\t', header=False)
+    df.to_csv('/tmp/fullcapture.csv', mode='a',  sep='\t', header=False)
 
-
-
+#add header to fullcapture csv
 col_Names=['Source', 'Destination', 'Destport', 'Sourceport', 'Size']
+Cov = pd.read_csv("/tmp/fullcapture.csv",  sep='\t', names=col_Names)
 
 
-Cov = pd.read_csv("/tmp/fulldump2.csv",  sep='\t', names=col_Names)
+print(localbroadcast)
+local_system_ips = (Convert(local_system_ip))
+myarray = np.asarray(local_system_ips)
 
 
-#localbroadcast = os.popen("ifconfig | grep broadcast | awk '{ print $6 }'").read()
+
+print("our local system IP's are {}".format(myarray))
+
+#remove packets addressed to broadcast address
+# THIS IS not working, so replace it with a for loop 
+# This is necessary because we also have to SUM each individual IP
+#Cov1 = Cov[Cov.Destination != (localbroadcast) ]
+#Cov1.to_csv('/tmp/withoutbroadcasts.csv')
+
+#Cov2 = Cov.loc[Cov['Destination'].isin([(myarray)])]
+#Cov2.to_csv('/tmp/cov2.csv')
 
 
-localbroadcast = "192.168.0.255"
+#inboundtraffic = Cov1.loc[Cov1['Source'].isin(local_system_ips)]
+#inboundtraffic.to_csv=('/tmp/inboundwithoutlocal.csv')
 
 
-Cov1 = Cov[Cov.Destination != (localbroadcast) ]
+#
+#
+#
+#Tiny up our workspace...
+#
+#
+#
+print("chmoding and moving csvs. . .")
+cmdline("cp /tmp/*csv /home/greenfan/Desktop/")
+cmdline("chmod 777 /home/greenfan/Desktop/*csv")
+print("clearing temp files. . . ")
+cmdline("rm -rf /tmp/*csv /tmp/*pcap")
 
-Cov.to_csv('/tmp/headerdump.csv')
-Cov1.to_csv('/tmp/withoutbroadcasts.csv')
-#print(Cov1)
 
+# Create a Dataframe from CSV
+#df1 = pd.read_csv('/tmp/headerdump.csv')
+
+#my_dataframe = my_dataframe[my_dataframe.employee_name != 'chad']
+
+# Create a Dataframe from CSV
+#    d1 = pd.read_csv('/tmp/fulldump.csv')
+
+    # Drop via logic: similar to SQL 'WHERE' clause
+#    d1 = d1(d1.Destination != '192.168.0.255')
+
+#    d1.to_csv('/tmp/dumpwithoutbroadcasts/csv', mode='a', header=False)
+    #print(df.loc[df['Destination'].isin(['192.168.0.255'])])
+
+
+
+
+#    df1.to_csv('/tmp/notbroadcasted.csv', mode='a', header=False)
+#
+
+
+
+# remove broadcast from Dest column
+#fulldump = pd.read_csv('/tmp/fulldump.csv')
+
+#df1 = df[~df['Dest'].str.contains('192.168.0.255')]
+
+#print(df1)
+#d1.to_csv('/tmp/d1.csv', mode='a', header=False)
+
+
+#df[df['Position'].str.contains("PG")]
+
+
+#ourlocalIP = os.system("ifconfig | grep -v 127 | grep inet\  | awk '{ print $2 }'")
+#, filter="not broadcast and not host {0}".format(localbroadcast)
+
+
+#Removed from middle
+
+#print(localbroadcast)
+#Cov1 = Cov[Cov.Destination != (localbroadcast) ]
+
+#Cov1.to_csv('/tmp/withoutbroadcasts.csv')
+
+#inboundsources = (Cov1['Source'])
+
+#outbounddestinations = (Cov1['Destination']et nu
